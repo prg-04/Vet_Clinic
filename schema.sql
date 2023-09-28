b02c38442a7b9cb69303e58dfc4fcbb860709b5e
prg-04 <karanjaevans95@gmail.com>
2023-09-28 18:45:04 +0300

    Add data

--- schema.sql
+++ schema.sql
@@ -1,12 +1,36 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+	id integer GENERATED ALWAYS AS IDENTITY, 
 	name VARCHAR(40),
 	date_of_birth DATE,
 	escape_attempts INTEGER,
 	neutered BOOLEAN,
 	weight_kg REAL,
+	PRIMARY KEY(id)
 )
 
+BEGIN;
 
+ALTER TABLE
+ADD COLUMN species VARCHAR(75);
+
+
+
+BEGIN;
+
+UPDATE animals
+SET species = 'Unspecified';
+
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%_mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species = 'Unspecified';
+
+COMMIT;
